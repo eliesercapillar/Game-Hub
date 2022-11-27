823601d2633b2985f577614338f0849b9d26c868
Elieser Capillar <elieser.capillar@hotmail.com>
2022-11-26 19:52:08 -0600

    Updated Animations

--- Comp 3020 - Milestone 3 - Group 32 - Game Hub/Assets/Imported Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
+++ Comp 3020 - Milestone 3 - Group 32 - Game Hub/Assets/Imported Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -33,26 +33,22 @@ namespace Cainos.PixelArtTopDown_Basic
             {
                 dir.x = -1;
                 animator.SetInteger("Direction", 3);
-                Debug.Log("Dir is: 3");
             }
             else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
                 dir.x = 1;
-                animator.SetInteger("Direction", 2);
-                Debug.Log("Dir is: 2");
+                animator.SetInteger("Direction", 2);        
             }
 
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
                 dir.y = 1;
                 animator.SetInteger("Direction", 1);
-                Debug.Log("Dir is: 1");
             }
             else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
                 dir.y = -1;
                 animator.SetInteger("Direction", 0);
-                Debug.Log("Dir is: 0");
             }
 
             dir.Normalize();
